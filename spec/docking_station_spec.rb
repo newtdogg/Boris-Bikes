b97dab542aeda3647e71b199e6f8a6a88b9c299f
newtdogg <edwardfrancisnewton@live.com>
2017-11-21 15:22:28 +0000

    refactored test, and dockingstation class now has release bike method to pass the test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,4 @@
-require "../lib/docking_station"
+require "./lib/docking_station"
 describe DockingStation do
-  it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 end
