75efb6d6bc56db39f479277632a98a5a9c21caf9
Tom Brandon <tom.j.v.brandon@gmail.com>
2017-11-21 16:14:36 +0000

    Added test for Bike class responding to working? and resolved in code in docking_station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,5 +4,6 @@ class DockingStation
 end
 
 class Bike
-
+  def working?
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,4 @@
 require "./lib/docking_station"
 describe Bike do
-
+  it { is_expected.to respond_to :working? }
 end
